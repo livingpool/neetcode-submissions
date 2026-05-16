41a78835554c9ce1817e8d02ac397a7666ff53f9
Tim Liu <52132459+livingpool@users.noreply.github.com>
2026-05-16 10:37:40 +0800

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int res = 0, n = heights.size();
+        int i = 0, j = n - 1;
+        while (i < j) {
+            int area = min(heights[i], heights[j]) * (j - i);
+            res = max(res, area);
+            if (heights[i] < heights[j]) i++;
+            else j--;
+        }
+        return res;
+    }
+};
