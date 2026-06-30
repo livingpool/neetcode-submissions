db43f9e498f0f51d574619f4273a6f1b8e72ee4e
Tim Liu <52132459+livingpool@users.noreply.github.com>
2026-06-30 10:47:35 +0800

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0, r = s.size() - 1;
+        while (l < r) {
+            if (!isalnum(s[l])) {
+                l++;
+                continue;
+            } else if (!isalnum(s[r])) {
+                r--;
+                continue;
+            }
+            if (tolower(s[l]) != tolower(s[r])) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
